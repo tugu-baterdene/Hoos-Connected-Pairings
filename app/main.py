85955274727754e058de962821a08e52df9c5030
tugu-baterdene <tugu.college@gmail.com>
2026-05-11 11:57:12 -0400

    Styling changes, adding CSS file

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import shutil
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from app.csv_loader import load_students_from_csv
 from app.csv_writer import write_pairings_to_csv
@@ -11,6 +12,7 @@ from app.pairing_logic import generate_pairings
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
